198610e973aba2c711b5e036adb3e0fcf6ba7ef2
Gonzalo Erreca <gonzaerreca@gmail.com>
2022-12-08 16:04:05 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(10, 20, 30);
+           }
+
+    public static int suma(int a,int b,int c) {
+        int sumatoria = a + b + c;
+        System.out.println(sumatoria);
+        return sumatoria;
+    }
+
+}
